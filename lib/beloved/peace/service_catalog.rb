c2c022412dfc9d87cee5ca91030e778dd7e4556f
Matt Darby <matt.darby@rackspace.com>
2016-01-15 18:53:39 -0500

    Memoize catalog

--- lib/beloved/peace/service_catalog.rb
+++ lib/beloved/peace/service_catalog.rb
@@ -11,21 +11,22 @@ class Peace::ServiceCatalog
   attr_accessor :services, :access_token
 
   def self.for(api_key, username)
+    @catalog ||= begin
+      # TODO: Remove this before very long
+      fog      = ::YAML.load_file("/Users/mdarby/.fog")
+      api_key  = fog[:default][:rackspace_api_key]
+      username = fog[:default][:rackspace_username]
+      # TODO: Remove this before very long
 
-    # TODO: Remove this before very long
-    fog      = ::YAML.load_file("/Users/mdarby/.fog")
-    api_key  = fog[:default][:rackspace_api_key]
-    username = fog[:default][:rackspace_username]
-    # TODO: Remove this before very long
+      headers  = {content_type: :json, accept: :json}
+      body     = { "auth": { "RAX-KSKEY:apiKeyCredentials": { "apiKey": api_key, "username": username } } }
+      response = ::RestClient.post(BASE_URL, body.to_json, headers)
+      body     = JSON.parse(response.body)
+      hash     = body['access']['serviceCatalog']
+      token    = body['access']['token']['id']
 
-    headers  = {content_type: :json, accept: :json}
-    body     = { "auth": { "RAX-KSKEY:apiKeyCredentials": { "apiKey": api_key, "username": username } } }
-    response = ::RestClient.post(BASE_URL, body.to_json, headers)
-    body     = JSON.parse(response.body)
-    hash     = body['access']['serviceCatalog']
-    token    = body['access']['token']['id']
-
-    Peace::ServiceCatalog.new(hash, token)
+      Peace::ServiceCatalog.new(hash, token)
+    end
   end
 
   def initialize(hash, token)
---
+++ spec/peace/service_catalog_spec.rb
@@ -0,0 +1,50 @@
+describe Peace::ServiceCatalog do
+  let(:service_catalog){ Peace::ServiceCatalog.for('x', 'y') }
+
+  it 'has many services' do
+    expect(service_catalog.services).to be_kind_of(Array)
+  end
+
+  it 'has an access token' do
+    expect(service_catalog.access_token).not_to be_empty
+  end
+
+  describe '#for' do
+    it 'returns a ServiceCatalog' do
+      expect(service_catalog).to be_kind_of(Peace::ServiceCatalog)
+    end
+  end
+
+  describe "Services" do
+    let(:service){ service_catalog.services.first }
+
+    it 'has a name' do
+      expect(service.name).not_to be_empty
+    end
+
+    it 'has endpoints' do
+      expect(service.endpoints).to be_kind_of(Array)
+    end
+
+
+    describe 'Endpoints' do
+      let(:endpoint){ service.endpoints.first }
+
+      it 'has a region' do
+        expect(endpoint.region).not_to be_empty
+      end
+
+      it 'has a tenant_id' do
+        expect(endpoint.tenant_id).not_to be_empty
+      end
+
+      it 'has a public_url' do
+        expect(endpoint.public_url).not_to be_empty
+      end
+
+      it 'has a internal_url' do
+        expect(endpoint.internal_url).to be_nil
+      end
+    end
+  end
+end
