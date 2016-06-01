6a645886d26de80b5919405f8478af854f2d2c02
Matt Darby <matt.darby@rackspace.com>
2016-06-01 19:18:35 -0400

    Peace refactored out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rackspace'
+require 'vcr'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -10,20 +11,16 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  unless Rackspace.mocking?
-    require 'vcr'
+  vcr_filters = %w{RS_API_KEY RS_USERNAME RS_TENANT_ID}
+  vcr_filters.each{ |env_var| raise "MISSING VCR SENSITIVE DATA ENV VAR: #{env_var}" unless ENV[env_var] }
 
-    vcr_filters = %w{RS_API_KEY RS_USERNAME RS_TENANT_ID}
-    vcr_filters.each{ |env_var| raise "MISSING VCR SENSITIVE DATA ENV VAR: #{env_var}" unless ENV[env_var] }
-
-    VCR.configure do |c|
-      c.cassette_library_dir = "spec/vcr"
-      c.hook_into :webmock
-      c.configure_rspec_metadata!
-      c.default_cassette_options = { :record => :new_episodes }
-      c.filter_sensitive_data('<RS_API_KEY>') { ENV['RS_API_KEY'] }
-      c.filter_sensitive_data('<RS_USERNAME>') { ENV['RS_USERNAME'] }
-      c.filter_sensitive_data('<RS_TENANT_ID>') { ENV['RS_TENANT_ID'] }
-    end
+  VCR.configure do |c|
+    c.cassette_library_dir = "spec/vcr"
+    c.hook_into :webmock
+    c.configure_rspec_metadata!
+    c.default_cassette_options = { :record => :new_episodes }
+    c.filter_sensitive_data('<RS_API_KEY>') { ENV['RS_API_KEY'] }
+    c.filter_sensitive_data('<RS_USERNAME>') { ENV['RS_USERNAME'] }
+    c.filter_sensitive_data('<RS_TENANT_ID>') { ENV['RS_TENANT_ID'] }
   end
 end
